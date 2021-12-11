7ec6874ff3f72fb865bb7a1accd447df15e48404
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-12-11 18:55:44 +0530

    updated app.js

--- app.js
+++ app.js
@@ -10,39 +10,34 @@ const path = require("path");
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json("hi");
-});
-
-app.get("/add", (req, res) => {
-  const query = req.query.query;
-  const movies = {
-    method: "GET",
-    url: "https://api.themoviedb.org/3/search/movie",
-    params: {
-      api_key: process.env.REACT_APP_TMDB_KEY,
-      language: "en-US",
-      page: 1,
-      include_adult: false,
-      query: query,
-    },
-  };
-
-  axios
-    .request(movies)
-    .then((response) => {
-      res.json(response.data.results);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-});
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     req.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
-}
 
+  app.get("/add", (req, res) => {
+    const query = req.query.query;
+    const movies = {
+      method: "GET",
+      url: "https://api.themoviedb.org/3/search/movie",
+      params: {
+        api_key: process.env.REACT_APP_TMDB_KEY,
+        language: "en-US",
+        page: 1,
+        include_adult: false,
+        query: query,
+      },
+    };
+
+    axios
+      .request(movies)
+      .then((response) => {
+        res.json(response.data.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+}
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
